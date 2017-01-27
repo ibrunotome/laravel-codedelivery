567a50c487c183b18475df7feeafd33d950c7ca5
Bruno Tomé <ibrunotome@gmail.com>
2017-01-27 11:27:08 -0200

    Orders.

---
+++ database/migrations/2017_01_27_131717_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('users');
+            $table->unsignedInteger('user_deliveryman_id')->nullable();
+            $table->foreign('user_deliveryman_id')->references('id')->on('users');
+            $table->decimal('total');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
