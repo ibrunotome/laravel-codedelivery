673ecc78779f5356096a3a327506dd3c6c533372
Bruno Tomé <ibrunotome@gmail.com>
2017-01-27 11:09:18 -0200

    Clients.

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function client()
+    {
+        return $this->hasOne(Client::class);
+    }
 }
